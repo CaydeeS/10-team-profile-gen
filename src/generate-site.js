98bf376853372796c90de4bfa486aeb2cdfbf5fc
Yed Star <chstokrp@gmail.com>
2022-04-18 15:21:38 -0700

    add more site generator

--- src/generate-site.js
+++ src/generate-site.js
@@ -61,6 +61,31 @@ const generateTeam = (team) => {
                         generateIntern(team[i]);
                     }    
                 }
-                
 
-}
+                return html.join('');
+            }
+
+            module.exports = team => {
+
+                return `
+                <!DOCTYPE html>
+              <html lang="en">
+              <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                    <link rel="stylesheet" href="..dist/style.css" />
+                    <title>Team Profile Generator</title>
+              </head>     
+              <body>
+                <header>
+                <h1> The Team </h1>
+                </header>
+                
+                <main> ${generateTeam(team)} </main>
+
+               </body> 
+               </html> 
+                    `;
+            }
